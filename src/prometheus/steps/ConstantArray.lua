3e793e27047805da4157886f7e28934d80296a46
aUxcDeVs <msnsmsstbrostuxk@gmail.com>
2025-10-11 04:26:27 +0800

    Update ConstantArray.lua

--- src/prometheus/steps/ConstantArray.lua
+++ src/prometheus/steps/ConstantArray.lua
@@ -315,7 +315,7 @@ function ConstantArray:apply(ast, pipeline)
 	self.rootScope = ast.body.scope;
 	self.arrId     = self.rootScope:addVariable();
 
-	self.base64chars = table.concat(util.shuffle{
+	self.base64chars = table.concat({
 		"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
 		"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
 		"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
