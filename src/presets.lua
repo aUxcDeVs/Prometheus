51ea0da22b93d5728d6c4072c84ec48022d89059
aUxcDeVs <msnsmsstbrostuxk@gmail.com>
2025-10-11 04:40:01 +0800

    Update presets.lua

--- src/presets.lua
+++ src/presets.lua
@@ -21,7 +21,7 @@ return {
             {
                 Name = "AntiTamper";
                 Settings = {
-                    UseDebug = true;
+                    UseDebug = false;
                 };
             },
             {
