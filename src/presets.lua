6bcf55a57714eb31e5c82a8e687a8e1ab5ffd693
aUxcDeVs <msnsmsstbrostuxk@gmail.com>
2025-10-11 01:33:33 +0800

    Update presets.lua

--- src/presets.lua
+++ src/presets.lua
@@ -11,7 +11,7 @@ return {
         -- For minifying no VarNamePrefix is applied
         VarNamePrefix = SK1D;
         -- Name Generator for Variables that look like this: IlI1lI1l
-        NameGenerator = "confuse";
+        NameGenerator = "mangled";
         -- No pretty printing
         PrettyPrint = false;
         -- Seed is generated based on current time
