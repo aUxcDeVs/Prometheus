9182fda9b8a29ee132385b49b350e8410dd2cef8
aUxcDeVs <msnsmsstbrostuxk@gmail.com>
2025-10-11 03:37:49 +0800

    Update steps.lua

--- src/prometheus/steps.lua
+++ src/prometheus/steps.lua
@@ -2,4 +2,5 @@ return {
 	WrapInFunction = require("prometheus.steps.WrapInFunction");
 	Vmify = require("prometheus.steps.Vmify");
 	ConstantArray = require("prometheus.steps.ConstantArray");
+	AntiTamper = require("prometheus.steps.AntiTamper");
 }
