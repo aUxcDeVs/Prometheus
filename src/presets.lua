e023530827c552f94ff32ba2d383844fdb47cae5
aUxcDeVs <msnsmsstbrostuxk@gmail.com>
2025-10-11 19:18:20 +0800

    Update presets.lua

--- src/presets.lua
+++ src/presets.lua
@@ -11,7 +11,7 @@ return {
         -- For minifying no VarNamePrefix is applied
         VarNamePrefix = "SHIZOnOBFUSCATORnABCABCBBCABCABAACABCABCA1ABCnFORKnnPROMETHEUS";
         -- Name Generator for Variables that look like this: IlI1lI1l
-        NameGenerator = "Il";
+        NameGenerator = "ConfuseII";
         -- No pretty printing
         PrettyPrint = false;
         -- Seed is generated based on current time
