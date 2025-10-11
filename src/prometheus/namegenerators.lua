77e93e8cc63e95b66e41a9e2211a4c1de565bbb2
aUxcDeVs <msnsmsstbrostuxk@gmail.com>
2025-10-11 19:17:20 +0800

    Update namegenerators.lua

--- src/prometheus/namegenerators.lua
+++ src/prometheus/namegenerators.lua
@@ -1,7 +1,10 @@
+--[[ Modified By Aux, Shizoscript , Best Credits ( Levno Owner of Prometheus Obfuscator i Fork! ]]
+	
 return {
 	Mangled = require("prometheus.namegenerators.mangled");
 	MangledShuffled = require("prometheus.namegenerators.mangled_shuffled");
 	Il = require("prometheus.namegenerators.Il");
 	Number = require("prometheus.namegenerators.number");
 	Confuse = require("prometheus.namegenerators.confuse");
+	ConfuseII = require("prometheus.namegenerators.confuse_II");
 }
