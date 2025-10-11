940f79406c5b628f1ea2d1125ab0d1bc0d18547e
aUxcDeVs <msnsmsstbrostuxk@gmail.com>
2025-10-11 20:30:18 +0800

    Update steps.lua

--- src/prometheus/steps.lua
+++ src/prometheus/steps.lua
@@ -3,4 +3,5 @@ return {
 	Vmify = require("prometheus.steps.Vmify");
 	ConstantArray = require("prometheus.steps.ConstantArray");
 	AntiTamper = require("prometheus.steps.AntiTamper");
+	NumbersToExpressions = require("prometheus.steps.NumbersToExpressions");
 }
