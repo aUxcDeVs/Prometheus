3b90f22543337e0bc688619041c26ad7c725a2c0
aUxcDeVs <msnsmsstbrostuxk@gmail.com>
2025-10-12 18:29:00 +0800

    Update confuse_II.lua

--- src/prometheus/namegenerators/confuse_II.lua
+++ src/prometheus/namegenerators/confuse_II.lua
@@ -2,16 +2,17 @@
 --
 -- namegenerators/mixed_toxic_shuffled.lua
 --
--- Mixed version: Toxic names + Il1 confusing characters + Shuffled mangled names
+-- Mixed version: Toxic names + Il1 confusing characters + Shuffled mangled names + Hex numbers
 
 local MIN_CHARACTERS = 5;
 local MAX_INITIAL_CHARACTERS = 20;
+local PREFIX = "_";
 
 local util = require("prometheus.util");
 local chararray = util.chararray;
 
 local offset = 0;
-local VarDigits = chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_Il1");
+local VarDigits = chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_Il1iIiIiIIiIiIiIiIIIII");
 local VarStartDigits = chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZIl");
 
 local varNames = {
@@ -106,10 +107,42 @@ local varNames = {
     "data_y",
     "ptr_a",
     "ptr_b",
+    "A_B_C_D",
+    "E_F_G_H",
+    "I_J_K_L",
+    "M_N_O_P",
+    "Q_R_S_T",
+    "U_V_W_X",
+    "Y_Z_A_B",
+    "C_D_E_F",
+    "G_H_I_J",
+    "K_L_M_N",
+    "O_P_Q_R",
+    "S_T_U_V",
+    "W_X_Y_Z",
+    "AA_BB_CC",
+    "DD_EE_FF",
+    "GG_HH_II",
+    "JJ_KK_LL",
+    "MM_NN_OO",
+    "PP_QQ_RR",
+    "SS_TT_UU",
+    "VV_WW_XX",
+    "YY_ZZ_AA",
+    "A1_B2_C3",
+    "D4_E5_F6",
+    "G7_H8_I9",
+    "J0_K1_L2",
+    "M3_N4_O5",
+    "P6_Q7_R8",
+    "S9_T0_U1",
+    "V2_W3_X4",
+    "Y5_Z6_A7",
+    "B8_C9_D0",
 }
 
 local function generateName(id, scope)
-    local choice = math.random(1, 3)
+    local choice = math.random(1, 4)
     
     if choice == 1 then
         -- Toxic names method
@@ -138,7 +171,7 @@ local function generateName(id, scope)
             name = name..confuseDigits[d+1]
         end
         return name
-    else
+    elseif choice == 3 then
         -- Shuffled mangled method
         local name = ''
         local d = id % #VarStartDigits
@@ -150,6 +183,9 @@ local function generateName(id, scope)
             name = name..VarDigits[d+1]
         end
         return name
+    else
+        -- Hex number method
+        return PREFIX .. tostring(id);
     end
 end
 
