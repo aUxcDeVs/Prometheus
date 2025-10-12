ac8b609ee8bd8f2184d97e585b01161cc07287be
aUxcDeVs <msnsmsstbrostuxk@gmail.com>
2025-10-12 16:31:57 +0800

    Much better

--- src/prometheus/namegenerators/confuse_II.lua
+++ src/prometheus/namegenerators/confuse_II.lua
@@ -1,8 +1,8 @@
 -- This Script is Part of the Prometheus Obfuscator by Levno_710
 --
--- namegenerators/toxic_confuse.lua
+-- namegenerators/mixed_toxic_shuffled.lua
 --
--- Mixed version: Toxic names + Il1 confusing characters
+-- Mixed version: Toxic names + Il1 confusing characters + Shuffled mangled names
 
 local MIN_CHARACTERS = 5;
 local MAX_INITIAL_CHARACTERS = 20;
@@ -11,11 +11,10 @@ local util = require("prometheus.util");
 local chararray = util.chararray;
 
 local offset = 0;
-local VarDigits = chararray("Il1");
-local VarStartDigits = chararray("Il");
+local VarDigits = chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_Il1");
+local VarStartDigits = chararray("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZIl");
 
 local varNames = {
-    
     "CPU",
     "IronBrewRust",
     "LuraphIsbetter",
@@ -33,8 +32,6 @@ local varNames = {
     "gay",
     "getout",
     "shizo",
-    
-    
     "loadstring",
     "getfenv",
     "setfenv",
@@ -68,8 +65,6 @@ local varNames = {
     "kiddo",
     "load",
     "rawequal",
-    
-
     "KSSJSJSNSISEJNISNJEJEJKSOSMKDNWKDN",
     "XKJDNEMSJDIEKSOPWENJDIEKDMSKDIE",
     "PLMKOWNSJDIEQWERBNASDDCVFZXCVBN",
@@ -85,22 +80,16 @@ local varNames = {
     "MNBVCXLKJHGFDSAQWERTYJUIOPLKJHG",
     "ERFVBGTHNMJUYIKOLOPMNJBHVGYCFT",
     "AWQZSXEDCRFTVGYBHUNJMIKOLPMKOI",
-    
-    
     "KSSJSJSNSISEJNISNJEJEJKSOSMKDNWKDNSKMWKDNPLMKOIUJNHBGYVFTCRDZEXSW",
     "XKJDNEMSJDIEKSOPWENJDIEKDMSKDIEPLOKIJUNHBGYVFTCRDZEXSWAQWZSXED",
     "PLMKOWNSJDIEQWERBNASDDCVFZXCVBNQWERTYASDFGHZXCVBNMNBVCXLKJHGF",
     "ZXCVBNQWERTYASDFGHHJKLZXCVBNMASDFGHJTYUIOPGHJKLZXCVBNMQAZWSX",
     "TYUIOPGHJKLZXCVBNMQAZWSXEDCRFVTGBYHNUJMIKOLPMJNHYGBVFTCDEXSW",
-    
-    
     "aGVsbG93b3JsZHRoaXNhaXNmZmFrZQ",
     "Y2hhdGdwdGR3cm90ZXN0YWNrb3Zlcg",
     "dGhpc2FpcxNzdXBlcmxvbmdmZmFrZQ",
     "eW91dGhpbmtdGhpc2FpcxNzb21l",
     "d2h5YXJleW91ZXZlbnjyZWFkaW5n",
-    
-    
     "var_a",
     "var_b",
     "tmp_x",
@@ -120,9 +109,10 @@ local varNames = {
 }
 
 local function generateName(id, scope)
+    local choice = math.random(1, 3)
     
-    if math.random() < 0.5 then
-        
+    if choice == 1 then
+        -- Toxic names method
         local name = {};
         local d = id % #varNames
         id = (id - d) / #varNames
@@ -133,10 +123,24 @@ local function generateName(id, scope)
             table.insert(name, varNames[d + 1]);
         end
         return table.concat(name, "_");
-    else
-        
+    elseif choice == 2 then
+        -- Il1 confusing method
         local name = ''
         id = id + offset;
+        local confuseDigits = chararray("Il1");
+        local confuseStart = chararray("Il");
+        local d = id % #confuseStart
+        id = (id - d) / #confuseStart
+        name = name..confuseStart[d+1]
+        while id > 0 do
+            local d = id % #confuseDigits
+            id = (id - d) / #confuseDigits
+            name = name..confuseDigits[d+1]
+        end
+        return name
+    else
+        -- Shuffled mangled method
+        local name = ''
         local d = id % #VarStartDigits
         id = (id - d) / #VarStartDigits
         name = name..VarStartDigits[d+1]
@@ -150,7 +154,6 @@ local function generateName(id, scope)
 end
 
 local function prepare(ast)
-    
     util.shuffle(varNames);
     util.shuffle(VarDigits);
     util.shuffle(VarStartDigits);
